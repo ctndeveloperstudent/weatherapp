fec9d9a53610bab6583ad824ffd11e01bcb038df
ctndeveloperstudent <15708@coderacademy.edu.au>
2024-07-10 12:19:14 +1000

    imported files

---
+++ src/index.js
@@ -0,0 +1,69 @@
+let now = new Date();
+
+function formatDate(date) {
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+  ];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"];
+
+  let currentDay = days[now.getDay()];
+  let currentDate = now.getDate();
+  let currentMonth = months[now.getMonth()];
+  let currentHour = now.getHours();
+  let currentMinute = now.getMinutes();
+
+  if (currentHour < 10) {
+    currentHour = `0${currentHour}`;
+  }
+
+  if (currentMinute < 10) {
+    currentMinute = `0${currentMinute}`;
+  }
+
+  let dateElement = document.querySelector("h2");
+  dateElement.innerHTML = `${currentDay}, ${currentDate} ${currentMonth} ${currentHour}:${currentMinute}`;
+}
+
+console.log(formatDate(now));
+
+// feature chane h1 when search
+function changeCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = searchInput.value;
+}
+
+let searchForm = document.querySelector("#searchform");
+searchForm.addEventListener("submit", changeCity);
+
+//change from celcius to f
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 66;
+}
+
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 19;
+}
+
+// Bonus Feature
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertToCelsius);
